8f489a593ce51e96bb87e3d6a7e49e5994fe999e
Option-Panda <optionpanda@protonmail.com>
2021-03-30 13:16:14 +0800

    make staking contract ownable

--- staking.sol
+++ staking.sol
@@ -4,7 +4,71 @@ pragma solidity ^0.6.12;
 
 import "library.sol";
 
-contract Staking {
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () internal {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+contract Staking is Ownable {
     using SafeERC20 for IERC20;
     using SafeMath for uint;
     
@@ -32,14 +96,6 @@ contract Staking {
     uint256 private _currentOPARound = 1;
     // @dev last OPA reward block
     uint256 private _lastRewardBlock = block.number;
-    
-    /**
-     * @dev Modifier to make a function callable only by owner
-     */
-    modifier onlyOwner() {
-        require(msg.sender == _owner, "restricted");
-        _;
-    }
     
     constructor(IERC20 opaContract, IERC20 assetContract, address owner) public {
         AssetContract = assetContract; 
